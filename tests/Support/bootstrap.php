55474d7fd6c50dec73856f1f230f2f0bc98cbcbd
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-11-23 07:10:29 -0300

    Initial commit. (#1)

---
+++ tests/Support/bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+define('YII_ENV', 'test');
+
+$root = dirname(__DIR__, 2);
+$autoload = $root . '/vendor/autoload.php';
+$yii2 = $root . '/vendor/yiisoft/yii2/Yii.php';
+
+if (!is_file($autoload)) {
+    die('You need to set up the project dependencies using Composer');
+}
+
+if (!is_file($yii2)) {
+    die('You need to set up yii2 using composer');
+}
+
+require_once $autoload;
+require_once $yii2;
